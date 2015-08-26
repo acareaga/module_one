3fd8dd914d85f3f3bad5d77dce49baac5b223b32
Aaron Careaga <aaroncareaga@gmail.com>
2015-08-26 10:22:06 -0600

    Warm-up practice on Allergy problem.

---
+++ week_one/allergy.rb
@@ -0,0 +1,12 @@
+allergy = {
+            :1 'eggs',
+            :2 'peanuts',
+            :4 'shellfish',
+            :8 'strawberries',
+            :16 'tomatoes',
+            :32 'chocolate',
+            :64 'pollen',
+            :128 'cats' }
+
+Jorge = peanuts + chocolate
+puts Jorge
