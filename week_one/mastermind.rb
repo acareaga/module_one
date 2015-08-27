8f0091d1d4643bf093d44d26eb2f01c8f3f6fd04
Aaron Careaga <aaroncareaga@gmail.com>
2015-08-27 08:23:33 -0600

    Reorg flow of Mastermind.

--- week_one/mastermind.rb
+++ week_one/mastermind.rb
@@ -1,7 +1,8 @@
 # markdown: https://github.com/turingschool/curriculum/blob/master/source/projects/mastermind.markdown
 # video: https://www.youtube.com/watch?v=OPeBUDXKGqA
+# instructions: http://www.hasbro.de/manuals/14151i02.pdf
 
-# 1. Welcome user to Mastermind - Enter main loop
+# 1. Welcome user to Mastermind - enter main loop
 # 2. Prompt game play, instructions, or quit
 # 3. If (i), display instructions
 # 4. If (q), break out of main loop. Display "Thanks for playing!"
@@ -14,36 +15,41 @@
 
 
 puts "Welcome to MASTERMIND."
-puts "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
-answer = gets.chomp
 
-play || "p" =
-instructions || i =
-quit || q =
+loop do
 
-secret = RRRR
-puts "The secret is #{secret}"
+  puts "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
+  answer = gets.chomp
 
-loop do # Main Game Loop
+  play || "p"
+  instructions || i
+  quit || q
+
+  secret = RRRR
+  puts "The secret is #{secret}"
+
+# Instructions
+  i = print instructions.chomp
+
+  instructions = "The objective is to break the secret code in the fewest number of guesses. Try to guess the exact colors and positions of the hidden Code pegs.
+
+  Red: you guessed a correct color in the correct position
+  White: you guessed a correct color in an incorrect position
+  Blank: you guessed an incorrect color"
+
+# Main Game Loop
+  loop do
   print "I have generated a beginner sequence with four elements made up of: (r)ed, (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game.
 
-        What's your guess?"
+      What's your guess?"
 
   guess = gets.chomp
 
-  # End Game
+# Winning Guess
   print "Congratulations! You guessed the correct sequence: #{guess}.
 
   Do you want to (p)lay again or (q)uit?"
 
+  end
+
 end
-
-
-# Instructions - http://www.hasbro.de/manuals/14151i02.pdf
-i = print instructions.chomp
-
-instructions = "The objective is to break the secret code in the fewest number of guesses. Try to guess the exact colors and positions of the hidden Code pegs.
-
-Red: you guessed a correct color in the correct position
-White: you guessed a correct color in an incorrect position
-Blank: you guessed an incorrect color"
