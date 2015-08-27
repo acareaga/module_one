c34047c58450f261ef628746d072d991c1a6927c
Aaron Careaga <aaroncareaga@gmail.com>
2015-08-27 08:11:56 -0600

    Abstract overview of Mastermind game, main loop and game loop.

--- week_one/mastermind.rb
+++ week_one/mastermind.rb
@@ -1,6 +1,18 @@
 # markdown: https://github.com/turingschool/curriculum/blob/master/source/projects/mastermind.markdown
 # video: https://www.youtube.com/watch?v=OPeBUDXKGqA
 
+# 1. Welcome user to Mastermind - Enter main loop
+# 2. Prompt game play, instructions, or quit
+# 3. If (i), display instructions
+# 4. If (q), break out of main loop. Display "Thanks for playing!"
+# 5. If (p), enter game play loop
+# 6. Prompt user to select a sequence of pegs
+# 7. Test guess against secret_pegs
+# 8. If correct, break game loop into "You Won!, play again?" message
+# 9. If incorrect, loop back to "user selection" #6
+# 10. Repeat 6-9 until winning selection or user quits
+
+
 puts "Welcome to MASTERMIND."
 puts "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
 answer = gets.chomp
