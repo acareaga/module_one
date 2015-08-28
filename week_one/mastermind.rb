212d16508c3e92544ddd7543fb71e1e1f3577c0b
Aaron Careaga <aaroncareaga@gmail.com>
2015-08-27 18:47:40 -0600

    Finished Map Pattern Test homework. Reworked Mastermind game.

--- week_one/mastermind.rb
+++ week_one/mastermind.rb
@@ -13,53 +13,59 @@
 # 9. If incorrect, loop back to "user selection" #6
 # 10. Repeat 6-9 until winning selection or user quits
 
-def prompt
-  # prompt game play, instructions, or quit
+# def prompt
+#   # prompt game play, instructions, or quit
+# end
+#
+# def mystery_sequence
+#   color = ["r", "g", "b", "y"]
+#   rand[color]
+#   # need to fix
+# end
+#
+# def user_guess
+#   # compare guess against secret code
+# end
+#
+# p mystery_sequence
+
+def play_game
+  print "I have generated a beginner sequence with four elements made up of: (r)ed, (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game.
+
+        What's your guess?"
+
+    guess = gets.chomp
 end
 
-def mystery_sequence
-  color = ["r", "g", "b", "y"]
-  rand[color]
-  # need to fix
-end
+play = "play"
+instructions = "instructions"
+quit = "quit"
 
-def user_guess
-  # compare guess against secret code
-end
-
-p mystery_sequence
-
-p "Welcome to MASTERMIND."
+puts "Welcome to MASTERMIND."
 
 loop do
 
   puts "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
   answer = gets.chomp
 
-  play || "p"
-  instructions || i
-  quit || q
+  if answer == "i"
+    puts "The objective is to break the secret code in the fewest number of guesses. Try to guess the exact colors and positions of the hidden Code pegs.
 
-# Instructions
-  instructions = "The objective is to break the secret code in the fewest number of guesses. Try to guess the exact colors and positions of the hidden Code pegs.
-
-  Red: you guessed a correct color in the correct position
-  White: you guessed a correct color in an incorrect position
-  Blank: you guessed an incorrect color"
-
-# Main Game Loop
-  loop do
-  print "I have generated a beginner sequence with four elements made up of: (r)ed, (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game.
-
-      What's your guess?"
-
-  guess = gets.chomp
-
-# Winning Guess
-  print "Congratulations! You guessed the correct sequence: #{guess}.
-
-  Do you want to (p)lay again or (q)uit?"
-
+    Red: you guessed a correct color in the correct position
+    White: you guessed a correct color in an incorrect position
+    Blank: you guessed an incorrect color"
+  elsif answer == "q"
+    break
+  else answer == "p"
+    play_game
   end
-
 end
+
+
+# # Winning Guess
+#   print "Congratulations! You guessed the correct sequence: #{guess}.
+#
+#   Do you want to (p)lay again or (q)uit?"
+
+
+# After generating random board:  next easiest step is how you can determine how many colors are in the correct position
