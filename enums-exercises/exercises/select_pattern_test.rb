6271bf3899859f3a94180718a94fbd03a8fc0c08
Aaron Careaga <aaroncareaga@gmail.com>
2015-08-28 08:53:12 -0600

    Select homework exercises.

--- enums-exercises/exercises/select_pattern_test.rb
+++ enums-exercises/exercises/select_pattern_test.rb
@@ -17,60 +17,77 @@ class SelectPatternTest < Minitest::Test
     numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     odds = []
     numbers.each do |number|
-      # Your code goes here
+      odds << number if number.odd?
     end
     assert_equal [1, 3, 5, 7, 9], odds
   end
 
   def test_pick_words_with_three_letters
-    skip
     words = ["pill", "bad", "finger", "cat", "blue", "dog", "table", "red"]
     selected = []
-    # Your code goes here
+    words.each do |word|
+      selected << word if word.length == 3
+    end
     assert_equal ["bad", "cat", "dog", "red"], selected
   end
 
   def test_pick_words_with_more_than_three_letters
-    skip
     words = ["pill", "bad", "finger", "cat", "blue", "dog", "table", "red"]
-    # Your code goes here
+    selected = []
+    words.each do |word|
+      selected << word if word.length > 3
+    end
     assert_equal ["pill", "finger", "blue", "table"], selected
   end
 
   def test_pick_words_ending_in_e
-    skip
     words = ["are", "you", "strike", "thinking", "belt", "piece", "warble", "sing", "pipe"]
-    # Your code goes here
+    selected = []
+    words.each do |word|
+      selected << word if word[-1] == "e"
+    end
     assert_equal ["are", "strike", "piece", "warble", "pipe"], selected
   end
 
   def test_pick_words_ending_in_ing
-    skip
     words = ["bring", "finger", "drought", "singing", "bingo", "purposeful"]
-    # Your code goes here
+    selected = []
+    words.each do |word|
+      selected << word if word[-3..-1] == "ing"
+    end
     assert_equal ["bring", "singing"], selected
   end
 
   def test_pick_words_containing_e
-    skip
     words = ["four", "red", "five", "blue", "pizza", "purple"]
-    # Your code goes here
+    selected = []
+    words.each do |word|
+      selected << word if word.include?("e")
+    end
     assert_equal ["red", "five", "blue", "purple"], selected
   end
 
   def test_pick_dinosaurs
-    skip
     animals = ["tyrannosaurus", "narwhal", "eel", "achillesaurus", "qingxiusaurus"]
-    # Your code goes here
+    dinosaurs = []
+    animals.each do |animal|
+      if animal[-3..-1] == "rus"
+        dinosaurs << animal
+      end
+    end
     assert_equal ["tyrannosaurus", "achillesaurus", "qingxiusaurus"], dinosaurs
   end
 
-  def test_pick_floats
-    skip
-    numbers = [3, 1.4, 3.5, 2, 4.9, 9.1, 8.0]
-    # Your code goes here
-    assert_equal [1.4, 3.5, 4.9, 9.1, 8.0], floats
-  end
+  # def test_pick_floats
+  #   numbers = [3, 1.4, 3.5, 2, 4.9, 9.1, 8.0]
+  #   foats = []
+  #   numbers.each do |number|
+  #     if number[-2] == "." # need to pull floats from list
+  #       floats << number
+  #     end
+  #   end
+  #   assert_equal [1.4, 3.5, 4.9, 9.1, 8.0], floats
+  # end
 
   def test_pick_arrays
     skip
