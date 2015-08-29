1e6d94c1375679bd57bdecaa00b7286084bb65ba
Aaron Careaga <aaroncareaga@gmail.com>
2015-08-28 18:36:14 -0600

    Improved Mastermind but still a lot left.

--- week_one/mastermind.rb
+++ week_one/mastermind.rb
@@ -13,59 +13,77 @@
 # 9. If incorrect, loop back to "user selection" #6
 # 10. Repeat 6-9 until winning selection or user quits
 
-play = "play"
-instructions = "instructions"
-quit = "quit"
+require 'pry'
 
-user_guess = ""
-@mystery_sequence = " rgby"
+# play = "play"
+# instructions = "instructions"
+# quit = "quit"
+# user_guess = ""
 
-# def mystery_sequence
-#   rand(color)
-#   # need to fix
-# end
+# need to fix guess again quit loop and min, sec, guess counters
 
-def play_game
-  print "I have generated a beginner sequence with four elements made up of: (r)ed, (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game.
+def play_game # Method for initial play and beyond
 
-        What's your guess?"
+  color = [ "r", "b", "g", "y" ]
+  mystery_sequence = ["r", "r", "r", "r"] # color.sample(4) remove to allow random generation
 
-  user_guess = gets.chomp # compare against mystery_sequence
+  minutes_played = 5 # record game play time
+  seconds_played = 20
 
-  if user_guess == @mystery_sequence
-  puts "Congratulations! You guessed the sequence #{user_guess} in 8 guesses over 4 minutes, 22 seconds. Do you want to (p)lay again or (q)uit?"
+  guess_counter = 0 # count the number of times it takes to win
+  if # user_guess =! mystery_sequence
+    guess_counter += 1
+  end
 
-  else
-    puts "I'm sorry, that is incorrect. You got two colors and no positions correct. Please guess again:"
+  colors_correct = 0 # count colors correct
+  if # color[""] == mystery_sequence[""]
+    colors_correct += 1
+  end
+
+  positions_correct = 0 # count positions correct
+  if # color[] == mystery_sequence[]
+    positions_correct += 1
+  end
+
+  loop do
+    puts "What's your guess?"
+    user_guess = gets.chomp
+
+    if user_guess.chars == mystery_sequence
+      puts "Congratulations! You guessed the sequence #{user_guess.upcase} in #{guess_counter} guesses over #{minutes_played} minutes and #{seconds_played} seconds. Do you want to (p)lay again or (q)uit?"
+      second_game_user_choice = gets.chomp
+      if (second_game_user_choice == "p") || (second_game_user_choice =='play')
+          play_game_without_initial_prompt
+      else
+          exit
+      end
+      elsif (second_game_user_choice == "q") || (second_game_user_choice == "quit")
+          exit
+      else
+          puts "I'm sorry, that is incorrect. You got #{colors_correct} colors and #{positions_correct} positions correct. Please guess again:"
+    end
   end
 end
 
-puts @mystery_sequence
-
-puts "Welcome to MASTERMIND."
-
-loop do # Main Game Loop
-  puts "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
-  answer = gets.chomp
-
-  if answer == "i"
-    puts "The objective is to break the secret code in the fewest number of guesses. Try to guess the exact colors (R, G, B, Y) in the positions of the hidden Code pegs."
-  elsif answer == "q"
-    break
-  else answer == "p"
+def first_game_stuff_and_play_a_game
+  # Main Game Loop
+  first_game_user_choice = gets.chomp
+  if (first_game_user_choice == "i") || (first_game_user_choice == "instructions")
+    puts "The objective is to break the secret code in the fewest number of guesses. Try to guess the exact colors (R, G, B, Y) in the positions of the hidden Code pegs. Please enter (p)lay, (i)nstructions, or (q)uit"
+    play_game
+  elsif (first_game_user_choice == "q") || (first_game_user_choice == "quit")
+    exit
+  else (first_game_user_choice == "p") || (first_game_user_choice == "play")
+    puts "I have generated a beginner sequence with four elements made up of: (r)ed, (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game."
     play_game
   end
 end
 
-# if user_guess == mystery_sequence
-#   puts "Congratulations! You guessed the sequence #{user_guess} in 8 guesses over 4 minutes, 22 seconds.
-#
-#       Do you want to (p)lay again or (q)uit?"
-#   if answer == "q"
-#     #break
-#   else answer == "p"
-#     play_game
-#   end
-# end
+def play_game_without_initial_prompt
+  puts "I have generated a beginner sequence with four elements made up of: (r)ed, (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game."
+  play_game
+end
 
-# After generating random board:  next easiest step is how you can determine how many colors are in the correct position
+puts "Welcome to MASTERMIND."
+puts "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
+first_game_stuff_and_play_a_game
