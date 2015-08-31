959a9a07051356d49ec445e72fa6cd4a5de9943f
Aaron Careaga <aaroncareaga@gmail.com>
2015-08-30 21:44:13 -0600

    Trying to fix color/position correct counter.

--- week_one/mastermind.rb
+++ week_one/mastermind.rb
@@ -25,7 +25,7 @@ def play_game # main sequence for game play
   @start_time = Time.now
   loop do
     puts "What's your guess?"
-    @user_guess = gets.chomp.downcase
+    @user_guess = gets.chomp.downcase.chars
     if @user_guess.chars == @mystery_sequence
       winning_sequence_play_again_or_quit
     elsif @user_guess.downcase.include?("p")
@@ -34,7 +34,18 @@ def play_game # main sequence for game play
       exit
     else
       @number_of_guesses += 1
-      puts "I'm sorry, that is incorrect. You got #{@colors_correct} colors and #{@positions_correct} positions correct. Please guess again."
+
+      positions_correct = []
+      colors_correct = []
+      @user_guess.each_with_index do |color, index|
+        if color == @mystery_sequence[color]
+          colors_correct += 1
+        else index == @mystery_sequence[index].to_i
+          positions_correct += 1
+        end
+      end
+
+      puts "I'm sorry, that is incorrect. You got #{colors_correct} colors and #{positions_correct} positions correct. Please guess again."
     end
   end
 end
@@ -42,10 +53,8 @@ end
 def invalid_guess_try_again
   if @user_guess.length < 4
     puts "Your guess is too short. Please enter a valid sequence."
-    play_game
   elsif @user_guess.length > 4
     puts "Your guess is too long. Please enter a valid sequence."
-    play_game
   else
     play_game
   end
@@ -54,7 +63,7 @@ end
 def first_game_info_and_play_game
   initial_user_response = gets.chomp
   if initial_user_response.downcase.include?("i")
-    puts "The objective is to break the secret code in the fewest number of guesses. \nTry to guess the exact colors (R, G, B, Y) in the positions of the hidden Code pegs."
+    puts "Instructions:\nThe objective is to break the secret code in the fewest number of guesses. \nTry to guess the exact colors - (r)ed, (g)reen, (b)lue, and (y)ellow - in the positions of the hidden code sequence."
     play_game
   elsif initial_user_response.downcase.include?("q")
     exit
@@ -72,7 +81,7 @@ end
 def winning_sequence_play_again_or_quit
   @end_time = Time.now
   minutes_played = @end_time.min - @start_time.min
-  seconds_played = @end_time.sec - @start_time.sec # need to fix +/- sec on min change
+  seconds_played = @start_time.sec + (@end_time.sec - @start_time.sec) # need to fix +/- sec on min change
   puts "Congratulations! \nYou guessed the sequence #{@user_guess.upcase} in #{@number_of_guesses} guesses over #{minutes_played} minutes and #{seconds_played} seconds. \nDo you want to (p)lay again or (q)uit?"
   play_again_or_quit = gets.chomp
   if play_again_or_quit.downcase.include?("q")
@@ -80,23 +89,23 @@ def winning_sequence_play_again_or_quit
   end
 end
 
-def colors_correct_counter
-  colors_correct = 0
-  @user_guess.each_with_index do |color, index|
-    if color == @mystery_sequence[color]
-      colors_correct += 1
-    end
-  end
-end
-
-def positions_correct_counter
-  positions_correct = 0
-  @user_guess.each_with_index do |color, index|
-    if index == @mystery_sequence[index]
-      positions_correct += 1
-    end
-  end
-end
+# def colors_correct_counter
+#   colors_correct = 0
+#   @user_guess.each_with_index do |color, index|
+#     if color == @mystery_sequence[color]
+#       colors_correct += 1
+#     end
+#   end
+# end
+#
+# def positions_correct_counter
+#   positions_correct = 0
+#   @user_guess.each_with_index do |color, index|
+#     if index == @mystery_sequence[index]
+#       positions_correct += 1
+#     end
+#   end
+# end
 
 puts "Welcome to MASTERMIND. \nWould you like to (p)lay, read the (i)nstructions, or (q)uit?"
 first_game_info_and_play_game
